829021457fccb55111f7bc21bcff80bf4257ea97
Umapyoi-yon <juan711219@gmail.com>
2026-05-18 22:11:22 -0700

    segundo commit

---
+++ escenas/player.gd
@@ -0,0 +1,35 @@
+class_name Player
+extends RigidBody3D
+var speed = 0.1
+var mouse = 0.01
+@onready var spring_arm_3d: SpringArm3D = $SpringArm3D
+
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		spring_arm_3d.rotation.x -= event.relative.y * mouse
+		spring_arm_3d.rotation.y -= event.relative.x * mouse
+		#spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, -PI/2, PI/4)
+		spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, deg_to_rad(-60), PI/4)
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spring_arm_3d.top_level = true
+	spring_arm_3d.spring_length = 5.0
+	spring_arm_3d.rotation_degrees.x = -20.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	spring_arm_3d.global_position = self.global_position
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var inputDirection = Vector3(
+		Input.get_axis("izquierda","derecha"),
+		0,
+		Input.get_axis("up","down")
+	)
+	inputDirection = inputDirection.rotated(Vector3.UP, spring_arm_3d.rotation.y).normalized() * speed
+	
+	apply_central_impulse(Vector3(inputDirection.x, 0, inputDirection.z))
